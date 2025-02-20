68bd8a327c0a9f6750fb85057af443ffd38d881c
LuizGustavo10 <gustavo180g@gmail.com>
2025-02-20 12:25:05 -0300

    App com banco funcionando

--- financas/lib/main.dart
+++ financas/lib/main.dart
@@ -1,14 +1,25 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
 }
 
 class Person {
+  String id;
   String name;
   String phone;
 
-  Person(this.name, this.phone);
+  Person({required this.id, required this.name, required this.phone});
+
+  factory Person.fromJson(String id, Map<String, dynamic> json) {
+    return Person(
+      id: id,
+      name: json['name'],
+      phone: json['phone'],
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -20,78 +31,86 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  final List<Person> people = [];
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Person> people = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPeople();
+  }
+
+  Future<void> _fetchPeople() async {
+    final url = Uri.parse(
+        'https://senac-b0fdc-default-rtdb.firebaseio.com/people.json');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic>? data = jsonDecode(response.body);
+      if (data != null) {
+        data.forEach((id, personData) {
+          setState(() {
+            people.add(Person.fromJson(id, personData));
+          });
+        });
+      }
+    }
+  }
+
+  Future<void> _deletePerson(String id) async {
+    final url = Uri.parse(
+        'https://senac-b0fdc-default-rtdb.firebaseio.com/people/$id.json');
+    final response = await http.delete(url);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        people.removeWhere((person) => person.id == id);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('App de Cadastro'),
+      appBar: AppBar(title: Text('App de Cadastro')),
+      body: ListView.builder(
+        itemCount: people.length,
+        itemBuilder: (context, index) {
+          final person = people[index];
+          return ListTile(
+            leading: Icon(Icons.person),
+            title: Text(person.name),
+            subtitle: Text(person.phone),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => _deletePerson(person.id),
+            ),
+          );
+        },
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            CardButton(
-              title: 'Cadastro de Pessoas',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CadastroPage(people: people)),
-                );
-              },
-            ),
-            SizedBox(height: 20),
-            CardButton(
-              title: 'Lista de Pessoas',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TabelaPage(people: people)),
-                );
-              },
-            ),
-            SizedBox(height: 20),
-            CardButton(
-              title: 'Sobre',
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SobrePage()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CardButton extends StatelessWidget {
-  final String title;
-  final VoidCallback onPressed;
-
-  CardButton({required this.title, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: ListTile(
-        title: Text(title, textAlign: TextAlign.center),
-        onTap: onPressed,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    CadastroPage(onPersonAdded: _fetchPeople)),
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
 class CadastroPage extends StatefulWidget {
-  final List<Person> people;
-
-  CadastroPage({required this.people});
+  final Function onPersonAdded;
+  CadastroPage({required this.onPersonAdded});
 
   @override
   _CadastroPageState createState() => _CadastroPageState();
@@ -101,12 +120,28 @@ class _CadastroPageState extends State<CadastroPage> {
   final _nameController = TextEditingController();
   final _phoneController = TextEditingController();
 
+  Future<void> _registerPerson() async {
+    final url = Uri.parse(
+        'https://senac-b0fdc-default-rtdb.firebaseio.com/people.json');
+    final response = await http.post(
+      url,
+      body: jsonEncode({
+        'name': _nameController.text,
+        'phone': _phoneController.text,
+      }),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      widget.onPersonAdded();
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cadastro de Pessoas'),
-      ),
+      appBar: AppBar(title: Text('Cadastro de Pessoas')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -115,73 +150,16 @@ class _CadastroPageState extends State<CadastroPage> {
               controller: _nameController,
               decoration: InputDecoration(labelText: 'Nome'),
             ),
-
             TextField(
               controller: _phoneController,
               decoration: InputDecoration(labelText: 'Telefone'),
             ),
-            
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  widget.people.add(Person(_nameController.text, _phoneController.text));
-                  _nameController.clear();
-                  _phoneController.clear();
-                });
-                Navigator.pop(context);
-              },
+              onPressed: _registerPerson,
               child: Text('Cadastrar'),
             ),
           ],
-        ),
-      ),
-    );
-  }
-} 
-
-class TabelaPage extends StatelessWidget {
-  final List<Person> people;
-
-  TabelaPage({required this.people});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista de Pessoas'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: people.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Icon(Icons.person),
-              title: Text(people[index].name),
-              subtitle: Text(people[index].phone),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class SobrePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sobre'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Text(
-            'Este é um aplicativo simples para cadastro de pessoas, exibindo nome e telefone.',
-            textAlign: TextAlign.center,
-          ),
         ),
       ),
     );
